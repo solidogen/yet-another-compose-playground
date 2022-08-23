f887c8c46afac4e5b6cce5dd82538eb94af456de
solidogen <solidogen@gmail.com>
2022-08-23 09:56:25 +0200

    Move app build logic to plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-}
-
-android {
-    compileSdk = libs.versions.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = libs.versions.applicationId.get()
-        minSdk = libs.versions.minSdk.get().toInt()
-        targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "0.0.1"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
-    }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+    id("internal-android-plugin")
 }
 
 dependencies {
--- buildSrc/src/main/kotlin/scripts/InternalAndroidPlugin.kt
+++ buildSrc/src/main/kotlin/scripts/InternalAndroidPlugin.kt
@@ -1,11 +1,85 @@
 package scripts
 
+import org.gradle.accessors.dm.LibrariesForLibs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.the
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import org.gradle.kotlin.dsl.findByType
 
 class InternalAndroidPlugin : Plugin<Project> {
 
+    private lateinit var project: Project
+    private val libs by lazy { project.the<LibrariesForLibs>() }
+
     override fun apply(target: Project) {
-        // todo
+        project = target
+        with(project) {
+            setupPlugins()
+            setupCommonAndroid()
+            setupAndroidApplication()
+        }
+    }
+
+    private fun Project.setupPlugins() {
+        plugins.apply("kotlin-android")
+    }
+
+    private fun Project.setupCommonAndroid() {
+        extensions.findByType<BaseExtension>()?.run {
+            setupAndroidDefaultConfig()
+//            setupCommonFlavors() // can be done here if needed
+        }
+    }
+
+    private fun BaseExtension.setupAndroidDefaultConfig() {
+        compileSdkVersion(libs.versions.compileSdk.get().toInt())
+
+        defaultConfig {
+            minSdk = libs.versions.minSdk.get().toInt()
+            targetSdk = libs.versions.targetSdk.get().toInt()
+
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+            consumerProguardFiles("consumer-rules.pro")
+
+            vectorDrawables {
+                useSupportLibrary = true
+            }
+
+            packagingOptions {
+                resources {
+                    excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                }
+            }
+        }
+    }
+
+    private fun Project.setupAndroidApplication() {
+        plugins.withId("com.android.application") {
+            extensions.findByType<BaseAppModuleExtension>()?.run {
+                defaultConfig {
+                    applicationId = libs.versions.applicationId.get()
+                    versionCode = 1
+                    versionName = "0.0.1"
+                }
+                buildTypes {
+                    getByName("release") {
+                        isMinifyEnabled = false
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+                buildFeatures {
+                    compose = true
+                }
+                composeOptions {
+                    kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+                }
+            }
+        }
     }
 }
